fb53aafeac8c3c54c23c6d406f87cde0d38c3661
zhuanglewei <zhuanglewei@gmail.com>
2018-12-05 22:38:57 -0800

    nothing

--- version.h
+++ version.h
@@ -1,6 +1,27 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 
+#include <ptlib.h>
+
+#define DEFAULT_PORT 1820
+
+class ProgConf
+{
+public:
+	ProgConf(WORD _port = DEFAULT_PORT):port(_port)
+	{};
+	enum GKMode{
+		NoGatekeeper,
+		RegisterWithGatekeeper
+	};
+	GKMode 		gkMode;
+	PString		gkAddr;
+	PString		gkId;
+	WORD		port;
+	PString		call;
+	PStringArray userAliases;	
+};
+
 #define MyMCU_VER_MAJOR 0
 #define MyMCU_VER_MINOR 1
 #define MyMCU_VER_STATUS ReleaseCode
